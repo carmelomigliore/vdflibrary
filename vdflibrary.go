357c0397a47576b9e60db2c9e00b0f5c1d564b6b
Cerato Jacopo <j.cerato@reply.it>
2020-06-08 17:42:33 +0200

    aggiunta variabile d'ambiente in funzione length

--- vdflibrary.go
+++ vdflibrary.go
@@ -16,7 +16,7 @@ import (
 	"github.com/mediocregopher/radix/v3"
 )
 
-var fileNames = [1]string{"./mnt/app/datalog.dat"}
+var fileNames []string
 var scanner *bufio.Scanner
 var err error
 var file *os.File
@@ -26,6 +26,7 @@ var state bool
 //Length function returns the array file names number of elements
 
 func Length() int {
+	fileNames = GetDirFilePaths(os.Getenv("DirectoryPath"))
 	return len(fileNames)
 }
 
